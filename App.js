9b2e38a7065e7f09997801d9f611733e3245bbcf
Antti Kylmanen <anttikk84@hotmail.com>
2021-03-24 12:50:16 +0200

    React native ex4

--- App.js
+++ App.js
@@ -1,21 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, Button, TouchableOpacity, Alert, TextInput, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import styles from './Styles';
+
+const notes= [
+  {
+    "id": 1,
+    "content": "note1"
+  },
+  {
+    "id": 2,
+    "content": "note2"
+  },
+  {
+    "id": 3,
+    "content": "note3"
+  },
+  {
+    "id": 4,
+    "content": "note4"
+  },
+  {
+    "id": 5,
+    "content": "note5"
+  },
+  {
+    "id": 6,
+    "content": "note6"
+  }
+]
+
+class NoteList extends React.Component {
+  state = {
+    notes: notes,
+    newNote:""
+  }
+
+  addNote=()=>{
+    const noteObject = {
+      content: this.state.newNote,
+      id: this.state.notes.length + 1
+    }
+
+    if (this.state.notes.filter(note => note.content.toLowerCase() === this.state.newNote.toLowerCase()).length > 0) {
+      Alert.alert("Note already there",
+      "Add another note",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ])
+
+    } else {
+      const notes = this.state.notes.concat(noteObject)
+
+      this.setState({
+      notes: notes,
+      newNote: ''
+      })
+    }
+
+    
+    
+    
+  }
+
+  handleNoteChange = (event) => {
+    this.setState({ newNote: event })
+  }
+
+
+  render(){
+    
+    return (
+      <View style={styles.noteContainer}>
+        <ScrollView style={styles.noteList}>
+            {this.state.notes.map(note => 
+              <Text key={note.id} style={styles.nList}>{note.content}</Text>
+              )}
+        </ScrollView>
+        <View style={styles.noteForm}>
+          <TextInput
+              placeholder="  Write a note here... "
+              value={this.state.newNote}
+              onChangeText={this.handleNoteChange}
+              style={styles.noteInput}
+          />
+            <View style = {styles.buttonContainer}>
+              <TouchableOpacity 
+                style={styles.noteButton}
+                onPress={this.addNote}
+              ><Text style = {styles.buttonText}>Add Note</Text></TouchableOpacity>
+            </View>
+
+            <View style = {styles.buttonContainer}>
+              <TouchableOpacity 
+                style={styles.noteButton}
+                onPress={() => this.props.navigation.navigate('AddNote')}
+              ><Text style = {styles.buttonText}>Add a new note</Text></TouchableOpacity>
+            </View>
+          
+
+        </View>
+        
+      </View>
+      
+
+
+
+    );
+  }
+
+}
+
+const AddNoteScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Add a new note</Text>
+
+      <TextInput
+        placeholder="Write a note here"
+      />
+      <Button
+        title="Add Note"
+      />
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Notes" screenOptions={{ headerStyle: {backgroundColor: '#ffa500',}}}>
+        <Stack.Screen name="ListNotes" component={NoteList}/>
+        <Stack.Screen name="AddNote" component={AddNoteScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+export default App;
